f80545415ca7a910998efcdf8719254400b18d10
Buck McCready <jbuckmccready@gmail.com>
2022-06-06 21:06:23 -0700

    Updated dependencies to eliminate security warning

--- src/integrators.rs
+++ src/integrators.rs
@@ -1,5 +1,8 @@
-use differential_systems::DifferentialSystem;
 use std::f64;
+
+use itertools::izip;
+
+use crate::differential_systems::DifferentialSystem;
 
 pub trait Integrator<T: DifferentialSystem> {
     fn do_step(&self, system: &T, x: &mut [f64; 4], t: &mut f64, h: &mut f64) -> i32;
@@ -8,12 +11,11 @@ pub trait Integrator<T: DifferentialSystem> {
 pub struct CashKarp54 {
     pub rel_tol: f64,
     pub abs_tol: f64,
-    pub max_step_size: f64
+    pub max_step_size: f64,
 }
 
 impl<T: DifferentialSystem> Integrator<T> for CashKarp54 {
     fn do_step(&self, system: &T, x: &mut [f64; 4], t: &mut f64, h: &mut f64) -> i32 {
-
         const C2: f64 = 1.0 / 5.0;
         const C3: f64 = 3.0 / 10.0;
         const C4: f64 = 3.0 / 5.0;
@@ -63,46 +65,104 @@ impl<T: DifferentialSystem> Integrator<T> for CashKarp54 {
         system.evaluate(&x, &mut k1, *t); // fill k1
 
         izip!((*x).iter(), k1.iter(), &mut temp_state)
-            .for_each(|(xi, k1i, ts)| { *ts = xi + *h *A21 * k1i });
+            .for_each(|(xi, k1i, ts)| *ts = xi + *h * A21 * k1i);
         let mut k2 = [0.0; 4];
         system.evaluate(&temp_state, &mut k2, *t + C2 * *h); // fill k2
 
         izip!((*x).iter(), k1.iter(), k2.iter(), &mut temp_state)
-            .for_each(|(xi, k1i, k2i, ts)| { *ts = xi + *h * (A31 * k1i + A32 * k2i) });
+            .for_each(|(xi, k1i, k2i, ts)| *ts = xi + *h * (A31 * k1i + A32 * k2i));
         let mut k3 = [0.0; 4];
         system.evaluate(&temp_state, &mut k3, *t + C3 * *h); // fill k3
 
-        izip!((*x).iter(), k1.iter(), k2.iter(), k3.iter(), &mut temp_state)
-            .for_each(|(xi, k1i, k2i, k3i, ts)| { *ts = xi + *h * (A41 * k1i + A42 * k2i + A43 * k3i) });
+        izip!(
+            (*x).iter(),
+            k1.iter(),
+            k2.iter(),
+            k3.iter(),
+            &mut temp_state
+        )
+        .for_each(|(xi, k1i, k2i, k3i, ts)| *ts = xi + *h * (A41 * k1i + A42 * k2i + A43 * k3i));
         let mut k4 = [0.0; 4];
         system.evaluate(&temp_state, &mut k4, *t + C4 * *h); // fill k4
 
-        izip!((*x).iter(), k1.iter(), k2.iter(), k3.iter(), k4.iter(), &mut temp_state)
-            .for_each(|(xi, k1i, k2i, k3i, k4i, ts)| { *ts = xi + *h * (A51 * k1i + A52 * k2i + A53 * k3i + A54 * k4i) });
+        izip!(
+            (*x).iter(),
+            k1.iter(),
+            k2.iter(),
+            k3.iter(),
+            k4.iter(),
+            &mut temp_state
+        )
+        .for_each(|(xi, k1i, k2i, k3i, k4i, ts)| {
+            *ts = xi + *h * (A51 * k1i + A52 * k2i + A53 * k3i + A54 * k4i)
+        });
         let mut k5 = [0.0; 4];
         system.evaluate(&temp_state, &mut k5, *t + C5 * *h); // fill k5
 
-        izip!((*x).iter(), k1.iter(), k2.iter(), k3.iter(), k4.iter(), k5.iter(), &mut temp_state)
-            .for_each(|(xi, k1i, k2i, k3i, k4i, k5i, ts)| { *ts = xi + *h * (A61 * k1i + A62 * k2i + A63 * k3i + A64 * k4i + A65 * k5i) });
+        izip!(
+            (*x).iter(),
+            k1.iter(),
+            k2.iter(),
+            k3.iter(),
+            k4.iter(),
+            k5.iter(),
+            &mut temp_state
+        )
+        .for_each(|(xi, k1i, k2i, k3i, k4i, k5i, ts)| {
+            *ts = xi + *h * (A61 * k1i + A62 * k2i + A63 * k3i + A64 * k4i + A65 * k5i)
+        });
         let mut k6 = [0.0; 4];
         system.evaluate(&temp_state, &mut k6, *t + C6 * *h); // fill k6
 
         let mut order5_solution = [0.0; 4];
-        izip!(k1.iter(), k2.iter(), k3.iter(), k4.iter(), k5.iter(), k6.iter(), &mut order5_solution)
-            .for_each(|(k1i, k2i, k3i, k4i, k5i, k6i, o5s)|{ *o5s = *h * (B5O1 * k1i + B5O2 * k2i + B5O3 * k3i + B5O4 * k4i + B5O5 * k5i + B5O6 * k6i) });
+        izip!(
+            k1.iter(),
+            k2.iter(),
+            k3.iter(),
+            k4.iter(),
+            k5.iter(),
+            k6.iter(),
+            &mut order5_solution
+        )
+        .for_each(|(k1i, k2i, k3i, k4i, k5i, k6i, o5s)| {
+            *o5s =
+                *h * (B5O1 * k1i + B5O2 * k2i + B5O3 * k3i + B5O4 * k4i + B5O5 * k5i + B5O6 * k6i)
+        });
 
-        izip!(k1.iter(), k2.iter(), k3.iter(), k4.iter(), k5.iter(), k6.iter(), &mut temp_state)
-            .for_each(|(k1i, k2i, k3i, k4i, k5i, k6i, ts)| { *ts = *h * (BDIFF1 * k1i + BDIFF2 * k2i + BDIFF3 * k3i + BDIFF4 * k4i + BDIFF5 * k5i + BDIFF6 * k6i) });
+        izip!(
+            k1.iter(),
+            k2.iter(),
+            k3.iter(),
+            k4.iter(),
+            k5.iter(),
+            k6.iter(),
+            &mut temp_state
+        )
+        .for_each(|(k1i, k2i, k3i, k4i, k5i, k6i, ts)| {
+            *ts = *h
+                * (BDIFF1 * k1i
+                    + BDIFF2 * k2i
+                    + BDIFF3 * k3i
+                    + BDIFF4 * k4i
+                    + BDIFF5 * k5i
+                    + BDIFF6 * k6i)
+        });
 
         let mut potential_solution = [0.0; 4];
         izip!((*x).iter(), order5_solution.iter(), &mut potential_solution)
-            .for_each(|(xi, o5s, ps)| { *ps = xi + o5s} );
+            .for_each(|(xi, o5s, ps)| *ps = xi + o5s);
 
         let mut error_values = [0.0; 4];
-        izip!(temp_state.iter(), potential_solution.iter(), &mut error_values)
-            .for_each(|(ts, ps, ev)| { *ev = (ts / (self.abs_tol + self.rel_tol * ps)).abs() });
+        izip!(
+            temp_state.iter(),
+            potential_solution.iter(),
+            &mut error_values
+        )
+        .for_each(|(ts, ps, ev)| *ev = (ts / (self.abs_tol + self.rel_tol * ps)).abs());
 
-        let max_error: f64 = error_values.iter().fold(f64::NAN, |a, b| if a > *b { a } else { *b });
+        let max_error: f64 = error_values
+            .iter()
+            .fold(f64::NAN, |a, b| if a > *b { a } else { *b });
 
         if max_error > 1.0 {
             *h *= (0.9 * max_error.powf(-0.25)).max(0.2);
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-extern crate time;
-extern crate image;
-#[macro_use] extern crate itertools;
-extern crate rayon;
+use std::time::Instant;
+
+use itertools::iproduct;
 use rayon::prelude::*;
-mod integrators;
 mod differential_systems;
+mod integrators;
 use differential_systems::*;
 use integrators::*;
-use time::{PreciseTime};
-use std::fs::File;
-use std::path::Path;
 
 pub struct IntegrationResult {
     pub converge_result: i32,
     pub converge_time: f64,
-    pub step_count: u32
+    pub step_count: u32,
 }
 
 #[inline]
-fn integrate_point<T: Integrator<PendulumSystem>>(integrator: &T, pendulum_system: &PendulumSystem, point: &mut [f64; 4]) -> IntegrationResult {
+fn integrate_point<T: Integrator<PendulumSystem>>(
+    integrator: &T,
+    pendulum_system: &PendulumSystem,
+    point: &mut [f64; 4],
+) -> IntegrationResult {
     fn is_near_attractor(attr_x: f64, attr_y: f64, curr_x: f64, curr_y: f64, tol: f64) -> bool {
-        (attr_x - tol < curr_x) && (curr_x < attr_x + tol) && (attr_y  - tol < curr_y) && (curr_y < attr_y + tol)
+        (attr_x - tol < curr_x)
+            && (curr_x < attr_x + tol)
+            && (attr_y - tol < curr_y)
+            && (curr_y < attr_y + tol)
     }
 
     fn is_near_mid(curr_x: f64, curr_y: f64, tol: f64) -> bool {
@@ -40,16 +43,29 @@ fn integrate_point<T: Integrator<PendulumSystem>>(integrator: &T, pendulum_syste
     let mut near_attractor = false;
     let attractor_count = pendulum_system.attractors.len();
     while trial_count < 1000 {
-        step_count += integrator.do_step(&pendulum_system, &mut *point, &mut curr_time, &mut step_size) as u32;
+        step_count += integrator.do_step(
+            &pendulum_system,
+            &mut *point,
+            &mut curr_time,
+            &mut step_size,
+        ) as u32;
         trial_count += 1;
         'inner: for i in 0..attractor_count {
-            near_attractor = is_near_attractor(pendulum_system.attractors[i].x_position,
-                                               pendulum_system.attractors[i].y_position,
-                                               point[0], point[1], pos_tol);
+            near_attractor = is_near_attractor(
+                pendulum_system.attractors[i].x_position,
+                pendulum_system.attractors[i].y_position,
+                point[0],
+                point[1],
+                pos_tol,
+            );
             if near_attractor {
                 if current_attractor == i as i32 {
                     if curr_time - initial_time_found > time_tol {
-                        return IntegrationResult { converge_result: current_attractor, converge_time: curr_time, step_count };
+                        return IntegrationResult {
+                            converge_result: current_attractor,
+                            converge_time: curr_time,
+                            step_count,
+                        };
                     }
                 } else {
                     current_attractor = i as i32;
@@ -62,19 +78,25 @@ fn integrate_point<T: Integrator<PendulumSystem>>(integrator: &T, pendulum_syste
         if !near_attractor && is_near_mid(point[0], point[1], mid_tol) {
             if current_attractor == -1 {
                 if curr_time - initial_time_found > time_tol {
-                    return IntegrationResult { converge_result: current_attractor, converge_time: curr_time, step_count };
+                    return IntegrationResult {
+                        converge_result: current_attractor,
+                        converge_time: curr_time,
+                        step_count,
+                    };
                 }
             } else {
                 current_attractor = -1;
                 initial_time_found = curr_time;
             }
-
         }
         near_attractor = false;
     }
-    IntegrationResult { converge_result: current_attractor, converge_time: curr_time, step_count }
+    IntegrationResult {
+        converge_result: current_attractor,
+        converge_time: curr_time,
+        step_count,
+    }
 }
-
 
 fn main() {
     let test_sys = PendulumSystem::new(
@@ -84,23 +106,44 @@ fn main() {
         0.2,
         10.0,
         vec![
-            Attractor {x_position: -0.5, y_position: 0.866_025_403_78, force_coefficient: 1.0},
-            Attractor {x_position: -0.5, y_position: -0.866_025_403_78, force_coefficient: 1.0},
-            Attractor {x_position: 1.0, y_position: 0.0, force_coefficient: 1.0}
-        ]
+            Attractor {
+                x_position: -0.5,
+                y_position: 0.866_025_403_78,
+                force_coefficient: 1.0,
+            },
+            Attractor {
+                x_position: -0.5,
+                y_position: -0.866_025_403_78,
+                force_coefficient: 1.0,
+            },
+            Attractor {
+                x_position: 1.0,
+                y_position: 0.0,
+                force_coefficient: 1.0,
+            },
+        ],
     );
 
-    let test_integrator = CashKarp54 { rel_tol: 1.0e-6, abs_tol: 1.0e-6, max_step_size: 0.1 };
-    let res = 0.15;
+    let test_integrator = CashKarp54 {
+        rel_tol: 1.0e-6,
+        abs_tol: 1.0e-6,
+        max_step_size: 0.1,
+    };
+    let res = 0.1;
     let extent = 7.5;
     let dim = (extent / res) as i32;
-    let mut imgbuf = image::ImageBuffer::new(2*dim as u32 + 1, 2*dim as u32 + 1);
+    let mut imgbuf = image::ImageBuffer::new(2 * dim as u32 + 1, 2 * dim as u32 + 1);
 
-    println!("integrating {0} points", (2*dim)*(2*dim));
-    let start = PreciseTime::now();
-    let mut points: Vec<[f64; 4]> = iproduct!(-dim..dim, -dim..dim).map(|(x, y)| { [f64::from(x) * res, f64::from(y) * res, 0.0, 0.0] }).collect();
+    println!("integrating {0} points", (2 * dim) * (2 * dim));
+    let start = Instant::now();
+    let mut points: Vec<[f64; 4]> = iproduct!(-dim..dim, -dim..dim)
+        .map(|(x, y)| [f64::from(x) * res, f64::from(y) * res, 0.0, 0.0])
+        .collect();
 
-    let results: Vec<IntegrationResult> = points.par_iter_mut().map(|point| { integrate_point(&test_integrator, &test_sys, point) }).collect();
+    let results: Vec<IntegrationResult> = points
+        .par_iter_mut()
+        .map(|point| integrate_point(&test_integrator, &test_sys, point))
+        .collect();
 
     let mut index = 0;
     for x in -dim..dim {
@@ -109,21 +152,19 @@ fn main() {
             let y_pixel = (dim - y) as u32;
             let result = &results[index];
             match result.converge_result {
-                -1 => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([255, 255, 255])),
+                -1 => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([255u8, 255, 255])),
                 0 => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([255, 140, 0])),
                 1 => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([30, 144, 255])),
                 2 => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([178, 34, 34])),
-                _ => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([0, 0, 0]))
+                _ => imgbuf.put_pixel(x_pixel, y_pixel, image::Rgb([0, 0, 0])),
             }
             index += 1;
         }
     }
 
-    let time = start.to(PreciseTime::now());
+    let time = Instant::now() - start;
 
-    let fout = &mut File::create(&Path::new("fractal.png")).unwrap();
+    imgbuf.save("fractal.png").unwrap();
 
-    let _    = image::ImageRgb8(imgbuf).save(fout, image::PNG);
-
-    println!("timing: {}", time.num_milliseconds());
+    println!("timing: {}ms", time.as_millis());
 }
